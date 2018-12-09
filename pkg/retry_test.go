bce0eb49321247e4b51920f5d4f5d3678beb3694
Adam Wasila <adam.wasila@gmail.com>
2018-12-09 12:46:16 +0100

    Add unit tests for constructor function

---
+++ pkg/retry_test.go
@@ -0,0 +1,34 @@
+package failsafe_test
+
+import (
+	"testing"
+	"time"
+
+	failsafe "github.com/adamwasila/gofailsafe/pkg"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyRetry(t *testing.T) {
+	_, err := failsafe.NewRetry()
+	assert.Nil(t, err, "Empty, no-op retry must be possible to create")
+}
+
+func TestRetryWithCustomDelay(t *testing.T) {
+	_, err := failsafe.NewRetry(failsafe.WithDelay(100 * time.Second))
+	assert.Nil(t, err, "Retry with custom delay must be possible to create")
+}
+
+func TestRetryWithCustomInvalidDelay(t *testing.T) {
+	_, err := failsafe.NewRetry(failsafe.WithDelay(-1 * time.Second))
+	assert.NotNil(t, err, "Retry with custom, negative delay must return error")
+}
+
+func TestRetryWithCustomRetries(t *testing.T) {
+	_, err := failsafe.NewRetry(failsafe.WithRetries(1))
+	assert.Nil(t, err, "Retry with custom number of retries must be possible to create")
+}
+
+func TestRetryWithCustomInvalidRetries(t *testing.T) {
+	_, err := failsafe.NewRetry(failsafe.WithRetries(-1))
+	assert.NotNil(t, err, "Retry with custom, negative number of retries must return error")
+}
